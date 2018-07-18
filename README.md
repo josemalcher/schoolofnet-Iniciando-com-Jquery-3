5bc13afb03ed439f6b39eb8fdc6dd10457e22be8
José Stélio R. Malcher Junior <contato@josemalcher.net>
2018-07-18 02:13:09 -0300

    + 6 - Show, Hide, Toggle

--- README.md
+++ README.md
@@ -202,6 +202,30 @@ $('ul').addClass('ul').append('<li>Adicionado Dinamicamente</li>');
 
 ## <a name="parte6">Show, Hide, Toggle</a>
 
+```js
+let  liHover = function(){
+    //$(this).addClass('li-hover');
+    //$(this).hide('slow');
+    //$(this).hide();
+    //$(this).hide(1000);
+    $(this).toggle('slow');
+    //$(this).show();
+}
+let liHoverOut = function(){
+    $(this).removeClass('li-hover');
+}
+let liClick = function () {
+    alert($(this).text());
+}
+
+$('ul').addClass('ul');
+
+$('li').hover(liHover, liHoverOut).click(liClick);
+
+$('li:even').text('Inpar').addClass('li-even');
+$('li:odd').text('Par').addClass('li-odd');
+$('ul').addClass('ul').append('<li>Adicionado Dinamicamente</li>');
+```
 
 [Voltar ao Índice](#indice)
 
--- main.js
+++ main.js
@@ -1,33 +1,21 @@
-/* $('ul').css({
-    'padding': 0,
-    'margin': 0,
-    'list-style': 'none'
-});
-$('li').text('Item impar').css('padding', '10px');
-$('li:even').text('Inpar').css('background', '#ccc');
-$('li:odd').text('Par').css({
-    'background': '#aaa',
-    'color': '#fff'
-}); */
 let  liHover = function(){
-    $(this).addClass('li-hover');
+    //$(this).addClass('li-hover');
+    //$(this).hide('slow');
+    //$(this).hide();
+    //$(this).hide(1000);
+    $(this).toggle('slow');
+    //$(this).show();
 }
 let liHoverOut = function(){
     $(this).removeClass('li-hover');
 }
-$('ul').addClass('ul');
-/* 
-$('li').hover(liHover,liHoverOut).click(function(){
+let liClick = function () {
     alert($(this).text());
-});
- */
-$('li').on('click', function(){
-    alert($(this).text());
-})
+}
 
-/* $('body').on('click', 'li', function(){
-    alert($(this).text());
-} ) */
+$('ul').addClass('ul');
+
+$('li').hover(liHover, liHoverOut).click(liClick);
 
 $('li:even').text('Inpar').addClass('li-even');
 $('li:odd').text('Par').addClass('li-odd');
